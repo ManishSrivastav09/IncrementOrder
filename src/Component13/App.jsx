54f07bd484354072f2953b600a9755aa0bc540e0
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2024-04-05 12:42:32 +0530

    first commit

--- src/Component13/App.jsx
+++ src/Component13/App.jsx
@@ -50,6 +50,8 @@ const App = () => {
           <p>github</p>
           <p>github</p>
           <p>github</p>
+          <p>github</p>
+          <p>github</p>
           <button onClick={increment}>Increment</button>
           <button onClick={decrement}>Decrement</button>
         </div>
